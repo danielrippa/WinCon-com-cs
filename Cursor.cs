809f6af4882370d3fbd34f94f727055803e9dce2
danielrippa <621091+danielrippa@users.noreply.github.com>
2025-01-13 14:33:30 -0300

    .

--- Cursor.cs
+++ Cursor.cs
@@ -19,6 +19,43 @@ namespace WinCon {
       Kernel32.SetConsoleCursorPosition(Handle, position);
     }
 
+    private Kernel32.COORD GetPosition() {
+      return Kernel32.GetScreenBufferInfo(Handle).dwCursorPosition;
+    }
+
+    public short Row {
+      get => GetPosition().Y;
+      set => Goto(Column, value);
+    }
+
+    public short Column {
+      get => GetPosition().X;
+      set => Goto(value, Row);
+    }
+
+    private Kernel32.CONSOLE_CURSOR_INFO Info {
+      get => Kernel32.GetCursorInfo(Handle);
+      set => Kernel32.SetConsoleCursorInfo(Handle, ref value);
+    }
+
+    public short Size {
+      get => (short)Info.dwSize;
+      set {
+        var info = Info;
+        info.dwSize = (uint)value;
+        Info = info;
+      }
+    }
+
+    public bool Visible {
+      get => Info.bVisible;
+      set {
+        var info = Info;
+        info.bVisible = value;
+        Info = info;
+      }
+    }
+
   }
 
 }
